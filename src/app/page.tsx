ffa5a2fb404d234d92a2a5a89bda3369ce2457e9
Jordi Martinez <jordiupc87@gmail.com>
2024-01-30 19:23:34 +0100

    Before react router

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <MaxWidthWrapper>
-      <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl"></div>
+      <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
+        <h1 className="text-4xl font-bold tracking-light text-gray-900 sm:text-6xl">
+          Welcome to BeTokenized - Your Gateway to Tokenization Without Hassle!
+          <span className="text-blue-600"> No Crypto knowledge requried</span>
+        </h1>
+        <p className="mt-6 text-lg max-w-prose text-muted-foreground">
+          BeTokenized is a user-friendly NFT platform designed for creators of
+          all backgrounds, where you can easily tokenize your digital creations
+          without any prior crypto or blockchain knowledge. No wallets required!{" "}
+        </p>
+        <div className="flex flex-col sm:flex-row gap-4 mt-6">
+          <Link href="/products"> Browse popular NFTs</Link>
+        </div>
+      </div>
     </MaxWidthWrapper>
   );
 }
