989d569a7f20817537128ff97cc1ea57f2d94d3c
Jordi Martinez <jordiupc87@gmail.com>
2024-01-30 18:43:38 +0100

    Component - MaxWithWrapper created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
-
-
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 
 export default function Home() {
   return (
-    <div> Hello world</div>
+    <MaxWidthWrapper>
+      <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl"></div>
+    </MaxWidthWrapper>
   );
 }
