e51c1b2787b4dcbbfbcec354599188e44c1bf8ae
JordiMartinezDev <jordiupc87@gmail.com>
2024-01-31 11:49:52 +0100

    Navbar - Logo added

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import MaxWidthWrapper from "../MaxWidthWrapper";
+import Link from "next/link";
+import { Icons } from "./Icons";
 
 type Props = {};
 
@@ -8,7 +10,16 @@ const Navbar = (props: Props) => {
     <div className="bg-white sticky z-50 top-0 inset-x-0 h-16">
       <header className="relative bg-white">
         <MaxWidthWrapper>
-          <div></div>
+          <div className="border-b border-gray-200">
+            <div className="flex h-16 items-center">
+              {/** To do: Mobile nav */}
+              <div className="ml-4 flex lg:ml-0">
+                <Link href="/">
+                  <Icons.logo className="h-10 w-10"></Icons.logo>
+                </Link>
+              </div>
+            </div>
+          </div>
         </MaxWidthWrapper>
       </header>
     </div>
