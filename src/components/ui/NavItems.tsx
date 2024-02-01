a87f01d4c3e5f962c52ddefefc64a16514481002
Jordi Martinez <jordiupc87@gmail.com>
2024-02-01 16:37:43 +0100

    Useffect escape fixed

--- src/components/ui/NavItems.tsx
+++ src/components/ui/NavItems.tsx
@@ -33,6 +33,12 @@ const NavItems = (props: Props) => {
         setActiveIndex(null);
       }
     };
+
+    document.addEventListener("keydown", handler);
+
+    return () => {
+      document.removeEventListener("keydown", handler);
+    };
   }, []);
 
   return (
