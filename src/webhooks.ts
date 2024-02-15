3287e4a6cc68a3546a76be567c028d3d58248fa6
JordiMartinezDev <jordiupc87@gmail.com>
2024-02-15 17:08:33 +0100

    tsconfig server updated

--- src/webhooks.ts
+++ src/webhooks.ts
@@ -81,6 +81,22 @@ export const stripeWebhookHandler = async (
     });
 
     // send receipt
+    try {
+      const data = await resend.emails.send({
+        from: "DigitalHippo <hello@joshtriedcoding.com>",
+        to: [user.email],
+        subject: "Thanks for your order! This is your receipt.",
+        html: ReceiptEmailHtml({
+          date: new Date(),
+          email: user.email,
+          orderId: session.metadata.orderId,
+          products: order.products as Product[],
+        }),
+      });
+      res.status(200).json({ data });
+    } catch (error) {
+      res.status(500).json({ error });
+    }
   }
 
   return res.status(200).send();
